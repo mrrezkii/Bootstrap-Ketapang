51781ec0090dc23f0f9af4af56e3354a42740a21
unknown <m.ilham.mubarak@gmail.com>
2019-08-15 23:25:01 +0700

    add landing page wisata, paket WIP

--- scripts/index.js
+++ scripts/index.js
@@ -32,6 +32,28 @@ function geoLocation(geocoder, mapResults) {
   });
 }
 
+// Owl Carousel
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:18,
+  items:2,
+  center:true,
+  responsive:{
+      0:{
+          items:1,
+          center:false
+      },
+      600:{
+          center:false,
+          items:2
+      },
+      1000:{
+          items:2
+      }
+  }
+})
+
+
 // Fetch News API
 
 const newsUrl =
@@ -54,3 +76,4 @@ axios.get(newsUrl).then(resp => {
     `);
   }
 });
+
