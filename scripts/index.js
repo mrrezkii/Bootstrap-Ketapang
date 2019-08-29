96df75d2dba8801ec5eaffb69a056210141ff10a
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2019-08-29 22:55:16 +0700

    add detail paket

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 // Mansory Libarary Initialization
-$(".gallery-detail").masonry({
-  itemSelector: ".grid-item",
+$('.gallery-detail').masonry({
+  itemSelector: '.grid-item',
   columnWidth: 350,
   gutter: 10
 });
 
 // AIzaSyDYTqP6EacKcUYlnQaaGs2TlrKwAhUonoY
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
+  const map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: -6.903363, lng: 107.6081381 },
     zoom: 16
   });
@@ -18,9 +18,9 @@ function initMap() {
 }
 
 function geoLocation(geocoder, mapResults) {
-  const address = $("#alamatWisata").text();
+  const address = $('#alamatWisata').text();
   geocoder.geocode({ address: address }, function(res, status) {
-    if (status == "OK") {
+    if (status == 'OK') {
       mapResults.setCenter(res[0].geometry.location);
       const marker = new google.maps.Marker({
         map: mapResults,
@@ -33,10 +33,10 @@ function geoLocation(geocoder, mapResults) {
 }
 
 // Owl Carousel
-$(".owl-carousel").owlCarousel({
+$('.owl-carousel').owlCarousel({
   loop: true,
   margin: 18,
-  items: 2,
+  items: 5,
   center: true,
   responsive: {
     0: {
@@ -45,7 +45,7 @@ $(".owl-carousel").owlCarousel({
     },
     600: {
       center: false,
-      items: 2
+      items: 3
     },
     1000: {
       items: 2
@@ -53,18 +53,18 @@ $(".owl-carousel").owlCarousel({
   }
 });
 
+$('.carousel-paket').owlCarousel();
+
 // Fetch News API
 
 const newsUrl =
-  "https://newsapi.org/v2/everything?apiKey=f8fd87d48cf746e0a817a4f7a21bafe4&q=bandung AND (wisata OR travel OR turis OR alam OR pemandangan)&language=id";
+  'https://newsapi.org/v2/everything?apiKey=f8fd87d48cf746e0a817a4f7a21bafe4&q=bandung AND (wisata OR travel OR turis OR alam OR pemandangan)&language=id';
 axios.get(newsUrl).then(resp => {
   for (let i = 0; i < 6; i++) {
-    $("#newsContainer").append(`
+    $('#newsContainer').append(`
     <div class="col-md-4 col-sm-6 col-xs-6">
     <a href="${resp.data.articles[i].url}" target="_blank">
-    <div class="news-card" style='background-image:url("${
-      resp.data.articles[i].urlToImage
-    }")'>
+    <div class="news-card" style='background-image:url("${resp.data.articles[i].urlToImage}")'>
         <div class="news-content">
                 <h5>${resp.data.articles[i].title}</h5>
                 <p>${resp.data.articles[i].publishedAt}</p>
